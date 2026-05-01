add6e75b6eeb4f2c3a1e3b1f45d0f7cf5fb58eed
Oscar Mischler <Sabo.fx@proton.me>
2026-05-01 12:25:54 +0200

    fix flake8

---
+++ build/lib/mazegen/__init__.py
@@ -0,0 +1,17 @@
+"""Mazegen - A reusable maze generator package."""
+
+from mazegen.generator import MazeGenerator
+from mazegen.generator import NORTH
+from mazegen.generator import EAST
+from mazegen.generator import SOUTH
+from mazegen.generator import WEST
+from mazegen.generator import ALL_WALLS
+
+__all__ = [
+    "MazeGenerator",
+    "NORTH",
+    "EAST",
+    "SOUTH",
+    "WEST",
+    "ALL_WALLS",
+]
